c8ec1c182a41b41615ab1f21acc8ea3ea775d044
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-01-22 22:54:21 +0100

    optimise and finish invert, clear and toggle functions

--- public/js/script.js
+++ public/js/script.js
@@ -4,34 +4,44 @@ let snares = new Array(16).fill(false);
 let hiHats = new Array(16).fill(false);
 let rideCymbals = new Array(16).fill(false);
 
-const toggleDrum = function(drums, index) {
-    if (!drums || index < 0 || index > 15) {
-        return;
+const getDrumName = (name) => {
+    switch (name) {
+        case 'kicks':
+            return kicks;
+        case 'snares':
+            return snares;
+        case 'hiHats':
+            return hiHats;
+        case 'rideCymbals':
+            return rideCymbals;
+        default:
+            return;
     }
-
-    if (drums === 'kicks') {
-        kicks[index] = true;
-    } else if (drums === 'snares') {
-        snares[index] = true;
-    } else if (drums === 'hiHats') {
-        hiHats[index] = true;
-    } else if (drums === 'rideCymbals') {
-        rideCymbals[index] = true;
-    } else {
-        return;
-    }
-    console.log(drums);
 };
 
-const clear = (drums) => {
-    drums.fill(false);
+const toggleDrum = (drumArrayName, index) => {
+    if (!drumArrayName || index < 0 || index > 15) {
+        return;
+    }
+    const drums = getDrumName(drumArrayName);
+    drums[index] = !drums[index];
+    //console.log(drums);
 };
 
-const invert = drums => {
-    // make method to get arrayname from arraystring (kicks instead of 'kicks')
+const clear = (drumArrayName) => {
+    const drums = getDrumName(drumArrayName);
+    if (drums) {
+        drums.fill(false);
+    }
+};
+
+const invert = (drumArrayName) => {
+    const drums = getDrumName(drumArrayName);
+    if (!drums) {
+        return;
+    }
     for (let i=0; i < drums.length; i++) {
         drums[i] = !drums[i];
-        console.log(drums[i]);
+        //console.log(drums[i]);
     }
 };
-
